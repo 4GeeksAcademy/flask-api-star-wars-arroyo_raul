dfc3fb547e0cde46c486d9a4a6f8e06436692907
arroyo-raul <arroyoguardiaraul@gmail.com>
2026-01-08 19:29:30 +0000

    Falta Favoritos

--- src/app.py
+++ src/app.py
@@ -3,20 +3,21 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 import os
 from flask import Flask, request, jsonify, url_for
+from sqlalchemy import select
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, Person, Planet, User
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,22 +28,58 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+
+@app.route('/people', methods=['GET'])
 def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    people = db.session.execute(select(Person)).scalars().all()
+    response_body = [person.serialize() for person in people]
+    if not response_body:
+        return jsonify({"error": "No people found"}), 404
+    return jsonify(response_body), 200
 
+@app.route('/people/<int:person_id>', methods=['GET'])
+def get_person(person_id):
+   person = db.session.get(Person, person_id)
+   if person is None:
+       return jsonify({"error": "Person not found"}), 404
+   return jsonify(person.serialize()), 200
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+
+    planets = db.session.execute(select(Planet)).scalars().all()
+    response_body = [planet.serialize() for planet in planets]
+    if not response_body:
+        return jsonify({"error": "No planets found"}), 404
+    return jsonify(response_body), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = db.session.get(Planet, planet_id)
+    if planet is None:
+        return jsonify({"error": "Planet not found"}), 404
+    return jsonify(planet.serialize()), 200
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = db.session.execute(select(User)).scalars().all()
+    response_body = [user.serialize() for user in users]
+    if not response_body:
+        return jsonify({"error": "Users not found"}), 404
     return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
--- src/models.py
+++ src/models.py
@@ -4,16 +4,50 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+class Person(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(120), unique=False, nullable=False)
+    height: Mapped[int] = mapped_column(nullable=False)
+    birth_year: Mapped[str] = mapped_column(String(7), nullable=False)
 
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "height": self.height,
+            "birth_year": self.birth_year,
             # do not serialize the password, its a security breach
         }
+
+class Planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    terrain: Mapped[str] = mapped_column(String(50), nullable=False)
+    population: Mapped[int] = mapped_column(nullable=False)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "terrain": self.terrain,
+            "population": self.population
+        }
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    first_name: Mapped[str] = mapped_column(String(20), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(40), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email
+        }
+    
+class Favorite(db.Model):
+    id: Mapped[int] = mapped_column(primery_key=True, autoincrement=True)
+    
